f0a175ad052894d25aab94ef0b09a6fe97abd0a1
Shawn Weeks <sweeks@weeksconsulting.us>
2021-02-01 14:58:55 -0600

    Fixed some vars

--- entrypoint.py
+++ entrypoint.py
@@ -8,7 +8,7 @@ BAMBOO_INSTALL_DIR = env['BAMBOO_INSTALL_DIR']
 gen_cfg('server.xml.j2', '{}/conf/server.xml'.format(BAMBOO_INSTALL_DIR))
 
 
-if 'CROWD_SSO_ENABLED' in env and env['CROWD_SSO_ENABLED'] == 'true':
+if 'ATL_CROWD_SSO_ENABLED' in env and env['ATL_CROWD_SSO_ENABLED'] == 'true':
     gen_cfg('login.ftl.j2',         '{}/atlassian-bamboo/login.ftl'.format(BAMBOO_INSTALL_DIR))
     gen_cfg('seraph-config.xml.j2', '{}/atlassian-bamboo/WEB-INF/classes/seraph-config.xml'.format(BAMBOO_INSTALL_DIR))
     gen_cfg('crowd.properties.j2',  '{}/xml-data/configuration/crowd.properties'.format(BAMBOO_HOME))
